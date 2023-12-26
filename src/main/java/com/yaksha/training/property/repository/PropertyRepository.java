c53dae78941c38297d803ec6b93510aa54999fc4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-26 15:36:09 +0530

    Delete src directory

--- src/main/java/com/yaksha/training/property/repository/PropertyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yaksha.training.property.repository;
-
-import com.yaksha.training.property.entity.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
